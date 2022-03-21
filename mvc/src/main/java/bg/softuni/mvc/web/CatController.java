2e8d5558dc5c585dcd1f1f18de4c53f5eea1e05e
rd-lachezar-balev <lachezar.balev@rewe-digital.com>
2022-03-21 19:02:58 +0200

    Initial version.

--- mvc/src/main/java/bg/softuni/mvc/web/CatController.java
+++ mvc/src/main/java/bg/softuni/mvc/web/CatController.java
@@ -1,7 +1,12 @@
 package bg.softuni.mvc.web;
 
+import bg.softuni.mvc.model.CatDTO;
 import bg.softuni.mvc.service.CatService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,20 +19,37 @@ public class CatController {
     this.catService = catService;
   }
 
-//  @GetMapping
-//  public String getAllCats() {
-//
-//  }
+  @GetMapping("/new")
+  public String newCat() {
+    return "new-cat";
+  }
 
-//  @GetMapping("/{id}")
-//  public String getCat() {
-//
-//  }
-//
-//  @PostMapping
-//  public String createCat() {
-//
-//  }
+  @PostMapping
+  public String createCat(CatDTO newCat) {
+
+    long catId = catService.createCat(newCat);
+
+    return "redirect:/cats/" + catId;
+  }
+
+  @GetMapping("/{id}")
+  public String getCat(Model model, @PathVariable("id") Long id) {
+    var cat = catService.getCatById(id).orElseThrow();
+    model.addAttribute("cat", cat);
+
+    return "cat";
+  }
+
+
+  @GetMapping
+  public String getAllCats(Model model) {
+    var allCats = catService.getAllCats();
+
+    model.addAttribute("cats", allCats);
+
+    return "all-cats";
+  }
+
 //
 //  @DeleteMapping
 //  public String deleteCat() {
