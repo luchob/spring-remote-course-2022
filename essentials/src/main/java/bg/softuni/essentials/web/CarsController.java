250da7ebe942a79a0642c9e09a5db2760a8b65c2
rd-lachezar-balev <lachezar.balev@gmail.com>
2022-03-30 15:48:14 +0300

    Various thymeleaf experiments

--- essentials/src/main/java/bg/softuni/essentials/web/CarsController.java
+++ essentials/src/main/java/bg/softuni/essentials/web/CarsController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/cars")
@@ -25,7 +26,11 @@ public class CarsController {
   }
 
   @GetMapping("/details")
-  public String details() {
+  public String details(@RequestParam("id") long id,
+      Model model) {
+
+    model.addAttribute("car", carService.findById(id).orElseThrow());
+
     return "details";
   }
 }
