6c742b5774dd631431827c9393a219c5511ea998
rd-lachezar-balev <lachezar.balev@gmail.com>
2022-04-01 08:56:17 +0300

    Various thymeleaf experiments

--- essentials/src/main/java/bg/softuni/essentials/web/CarsController.java
+++ essentials/src/main/java/bg/softuni/essentials/web/CarsController.java
@@ -1,11 +1,15 @@
 package bg.softuni.essentials.web;
 
+import bg.softuni.essentials.model.CarDTO;
 import bg.softuni.essentials.service.CarService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/cars")
@@ -32,5 +36,12 @@ public class CarsController {
     model.addAttribute("car", carService.findById(id).orElseThrow());
 
     return "details";
+  }
+
+  @CrossOrigin("https://www.google.com/")
+  @ResponseBody
+  @GetMapping("/{id}")
+  public CarDTO findCarById(@PathVariable("id") Long id) {
+    return carService.findById(id).orElseThrow();
   }
 }
