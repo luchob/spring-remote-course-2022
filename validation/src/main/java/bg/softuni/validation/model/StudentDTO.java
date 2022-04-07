a4f6d210086abb43af211e4c57c77ed94c6d7cf1
rd-lachezar-balev <lachezar.balev@gmail.com>
2022-04-07 19:18:55 +0300

    demo2

--- validation/src/main/java/bg/softuni/validation/model/StudentDTO.java
+++ validation/src/main/java/bg/softuni/validation/model/StudentDTO.java
@@ -1,12 +1,17 @@
 package bg.softuni.validation.model;
 
 import java.time.LocalDate;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class StudentDTO {
 
+  @NotNull
+  @Size(min=5, max = 50)
   private String name;
   private LocalDate birthday;
   private SexEnum sex;
+  private String email;
 
   public String getName() {
     return name;
@@ -35,12 +40,22 @@ public class StudentDTO {
     return this;
   }
 
+  public String getEmail() {
+    return email;
+  }
+
+  public StudentDTO setEmail(String email) {
+    this.email = email;
+    return this;
+  }
+
   @Override
   public String toString() {
     return "StudentDTO{" +
         "name='" + name + '\'' +
         ", birthday=" + birthday +
         ", sex=" + sex +
+        ", email='" + email + '\'' +
         '}';
   }
 }
--- validation/src/main/java/bg/softuni/validation/web/StudentController.java
+++ validation/src/main/java/bg/softuni/validation/web/StudentController.java
@@ -1,20 +1,28 @@
 package bg.softuni.validation.web;
 
 import bg.softuni.validation.model.StudentDTO;
+import javax.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/students")
 public class StudentController {
 
+  private static final Logger LOGGER = LoggerFactory.
+      getLogger(StudentController.class);
+
   @ModelAttribute("studentModel")
   public void initModel(Model model) {
-    model.addAttribute("studentModel", model);
+    model.addAttribute("studentModel", new StudentDTO());
   }
 
   @GetMapping("/add")
@@ -23,8 +31,20 @@ public class StudentController {
   }
 
   @PostMapping("/add")
-  public String add(@ModelAttribute("studentModel")
-      StudentDTO studentModel) {
+  public String add(@Valid @ModelAttribute("studentModel") StudentDTO studentModel,
+      BindingResult bindingResult,
+      RedirectAttributes rAtt) {
+
+    if(bindingResult.hasErrors()){
+
+      LOGGER.info("Student received: {}", studentModel);
+
+      rAtt.addFlashAttribute("studentModel", studentModel);
+      rAtt.addFlashAttribute("org.springframework.validation.BindingResult.studentModel", studentModel);
+      return "redirect:/student-add/add";
+    }
+
+
     return "student-add";
   }
 
