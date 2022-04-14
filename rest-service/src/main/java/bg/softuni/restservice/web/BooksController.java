d1109806d19950cd2a2584e69383420e9ba96cc4
rd-lachezar-balev <lachezar.balev@gmail.com>
2022-04-14 18:42:34 +0300

    Cors allowed

--- rest-service/src/main/java/bg/softuni/restservice/web/BooksController.java
+++ rest-service/src/main/java/bg/softuni/restservice/web/BooksController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@CrossOrigin("*")
 @RequestMapping("/api/books")
 @RestController
 public class BooksController {
