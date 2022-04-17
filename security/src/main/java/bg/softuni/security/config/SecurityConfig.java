3c2b0876f995aed897f320a524bc8d0f280a7b3f
rd-lachezar-balev <lachezar.balev@gmail.com>
2022-04-17 16:52:13 +0300

    User detials service.

--- security/src/main/java/bg/softuni/security/config/SecurityConfig.java
+++ security/src/main/java/bg/softuni/security/config/SecurityConfig.java
@@ -1,13 +1,26 @@
 package bg.softuni.security.config;
 
 import bg.softuni.security.model.enums.UserRoleEnum;
+import bg.softuni.security.service.impl.AppUserDetailsService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  private final AppUserDetailsService userDetailsService;
+  private final PasswordEncoder passwordEncoder;
+
+  public SecurityConfig(AppUserDetailsService userDetailsService,
+      PasswordEncoder passwordEncoder) {
+    this.userDetailsService = userDetailsService;
+    this.passwordEncoder = passwordEncoder;
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -25,6 +38,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         antMatchers("/pages/admins").hasRole(UserRoleEnum.ADMIN.name()).
         // any remaining reqests should be authenticated
         anyRequest().
-        authenticated();
+        authenticated().
+        and().
+    formLogin().
+        loginPage("/users/login").
+        usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY).
+        usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY).
+        defaultSuccessUrl("/").
+        failureForwardUrl("/users/login-error").
+        and().
+    logout().
+        logoutUrl("/user/logout").
+        logoutSuccessUrl("/").
+        invalidateHttpSession(true).
+        deleteCookies("JSESSIONID");
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.
+        userDetailsService(userDetailsService).
+        passwordEncoder(passwordEncoder);
   }
 }
