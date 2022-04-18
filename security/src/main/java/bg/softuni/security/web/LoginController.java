b403590692a2a339fa922189156a4234abb715e3
rd-lachezar-balev <lachezar.balev@gmail.com>
2022-04-18 20:11:30 +0300

    Finalize security

--- security/src/main/java/bg/softuni/security/web/LoginController.java
+++ security/src/main/java/bg/softuni/security/web/LoginController.java
@@ -1,7 +1,12 @@
 package bg.softuni.security.web;
 
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
@@ -9,6 +14,18 @@ public class LoginController {
   @GetMapping("/users/login")
   public String login() {
     return "auth-login";
+  }
+
+  @PostMapping("/users/login-error")
+  public String onFailedLogin(
+      @ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY) String userName,
+      RedirectAttributes redirectAttributes) {
+
+    redirectAttributes.addFlashAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY, userName);
+    redirectAttributes.addFlashAttribute("bad_credentials", true);
+
+    return "redirect:/users/login";
+
   }
 
 }
