fd5a9a86d1bbdf96d8ea364df8325d5ed023eecb
rd-lachezar-balev <lachezar.balev@gmail.com>
2022-04-22 09:15:13 +0300

    HAL Explorer

--- hateoas/src/main/java/bg/softuni/hateoas/model/repository/StudentRepository.java
+++ hateoas/src/main/java/bg/softuni/hateoas/model/repository/StudentRepository.java
@@ -1,9 +1,16 @@
 package bg.softuni.hateoas.model.repository;
 
 import bg.softuni.hateoas.model.entity.StudentEntity;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
+@RepositoryRestResource(
+    path = "students-data"
+)
 @Repository
 public interface StudentRepository extends JpaRepository<StudentEntity, Long> {
+  List<StudentEntity> findAllByNameContains(@Param("partOfName") String partOfName);
 }
