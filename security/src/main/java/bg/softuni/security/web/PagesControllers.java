7bd25821cf204ce3aab24b84db9b7c75ce6ae9e4
rd-lachezar-balev <lachezar.balev@gmail.com>
2022-05-02 17:22:14 +0300

    Some more test cases.

--- security/src/main/java/bg/softuni/security/web/PagesControllers.java
+++ security/src/main/java/bg/softuni/security/web/PagesControllers.java
@@ -1,6 +1,5 @@
 package bg.softuni.security.web;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
